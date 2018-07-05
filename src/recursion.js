e00902daa94d4f85e324e55a1a035881bc456d57
solisbrayan <solisbryan138@gmail.com>
2018-07-05 14:54:34 -0500

    fixed indentation

--- src/recursion.js
+++ src/recursion.js
@@ -7,13 +7,13 @@
 // Example: 5! = 5 x 4 x 3 x 2 x 1 = 120
 // factorial(5); // 120
 var factorial = function(n) {
-if(n < 0){
-  return null;
-}if(n === 0){
-  return 1;
-}
-return n*factorial(n -1 )
-
+  if(n < 0){
+    return null;
+  }
+  if(n === 0){
+    return 1;
+  }
+  return n*factorial(n -1 )
 };
 // 2. Compute the sum of an array of integers.
 // sum([1,2,3,4,5,6]); // 21
